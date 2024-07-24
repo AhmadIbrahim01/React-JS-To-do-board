623fb6cf7acca52e3b4ec22bac386688163fdd83
Ahmad Ibrahim <99222724+AhmadIbrahim01@users.noreply.github.com>
2024-07-24 12:18:38 +0300

    Updated components

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,31 @@
 import { useState } from 'react'
 import './App.css'
 import Inpt from './Components/Inpt';
+import Board from './Components/Board';
 function App() {
 
   const [taskList, setTaskList] = useState([]);
 
 
   return (
+    <div className='px-12'>
       <div className="flex flex-col items-center justify-center py-8 gap-4">
         <h1 className="text-xl font-semibold">To Do Board Using React.JS</h1>
         <Inpt taskList={taskList} setTaskList={setTaskList} />
-        <div>
+      </div>
+      <div className='flex flex-col gap-4 sm:grid sm:grid-cols-3 px-4 sm:px-8 md:px-10 lg:px-12'>
           {taskList.map((task, index)=>
-            <li key={index}>{task}</li>
+           <Board
+            key={index}
+            index={index}
+            task={task}
+            taskList={taskList}
+            setTaskList={setTaskList}
+
+           />
           )}
         </div>
-      </div>
+    </div>
   )
 }
 
