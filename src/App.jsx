99eb8649ea00119397a9e469c5c538432e4408f2
Ahmad Ibrahim <99222724+AhmadIbrahim01@users.noreply.github.com>
2024-07-24 11:55:32 +0300

    Add Input Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
+import { useState } from 'react'
 import './App.css'
-
+import Inpt from './Components/Inpt';
 function App() {
 
+  const [taskList, setTaskList] = useState([]);
+
+
   return (
-    <>
-      <h1>To Do Board Using React.JS</h1>
-    </>
+      <div className="flex flex-col items-center justify-center py-8 gap-4">
+        <h1 className="text-xl font-semibold">To Do Board Using React.JS</h1>
+        <Inpt taskList={taskList} setTaskList={setTaskList} />
+        <div>
+          {taskList.map((task, index)=>
+            <li key={index}>{task}</li>
+          )}
+        </div>
+      </div>
   )
 }
 
